0a95586d2491417e1ff68c203914d99306deabdf
unknown <dave8git@gmail.com>
2021-09-02 13:39:10 +0200

    fix pseudoelement adjustment

--- src/js/data.js
+++ src/js/data.js
@@ -3,7 +3,7 @@ export const dataSource = {}; // eslint-disable-line no-unused-vars
 dataSource.coffee = [
   {
     id: 1,
-    class: 'products__picture--left',
+    
     sideHeading: '01. LA LAGARTIJA ',
     sideText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, cum tempora iure exercitationem id excepturi odio mollitia expedita.',
     roastingScore: '2/8',
